231ab404ca7d0d630dabba5836e65936bd982581
Ela Korkmaz <elakorkmaz87@gmail.com>
2016-10-14 15:20:56 +0200

    first version appointment

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-var express = require('express');
-var app = express();
+$( function() {
+  $('#datepicker').datepicker({ minDate: 0, maxDate: "+12M" });
+} );
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+$( function() {
+$.widget( "ui.timespinner", $.ui.spinner, {
+  options: {
+    // seconds
+    step: 60 * 1000,
+    // hours
+    page: 60
+  },
+
+  _parse: function( value ) {
+    if ( typeof value === "string" ) {
+      // already a timestamp
+      if ( Number( value ) == value ) {
+        return Number( value );
+      }
+      return +Globalize.parseDate( value );
+    }
+    return value;
+  },
+
+  _format: function( value ) {
+    return Globalize.format( new Date(value), "t" );
+  }
 });
 
-app.listen(3000);
+$( "#spinner" ).timespinner();
 
-console.log("yoga");
+$( "#culture" ).on( "change", function() {
+  var current = $( "#spinner" ).timespinner( "value" );
+  Globalize.culture( $(this).val() );
+  $( "#spinner" ).timespinner( "value", current );
+});
+} );
