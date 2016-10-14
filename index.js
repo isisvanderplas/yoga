08404bd72074d3f2735d86f442a0bf53e0f32416
Ela Korkmaz <elakorkmaz87@gmail.com>
2016-10-14 16:50:54 +0200

    test

--- index.js
+++ index.js
@@ -3,35 +3,28 @@ $( function() {
 } );
 
 $( function() {
-$.widget( "ui.timespinner", $.ui.spinner, {
-  options: {
-    // seconds
-    step: 60 * 1000,
-    // hours
-    page: 60
-  },
+  $.widget( "ui.timespinner", $.ui.spinner, {
+    options: {
+      // seconds
+      step: 60 * 1000,
+      // hours
+      page: 60
+    },
 
-  _parse: function( value ) {
-    if ( typeof value === "string" ) {
-      // already a timestamp
-      if ( Number( value ) == value ) {
-        return Number( value );
+    _parse: function( value ) {
+      if ( typeof value === "string" ) {
+        // already a timestamp
+        if ( Number( value ) == value ) {
+          return Number( value );
+        }
+        return +Globalize.parseDate( value );
       }
-      return +Globalize.parseDate( value );
+      return value;
+    },
+
+    _format: function( value ) {
+      return Globalize.format( new Date(value), "t" );
     }
-    return value;
-  },
+  });
 
-  _format: function( value ) {
-    return Globalize.format( new Date(value), "t" );
-  }
-});
-
-$( "#spinner" ).timespinner();
-
-$( "#culture" ).on( "change", function() {
-  var current = $( "#spinner" ).timespinner( "value" );
-  Globalize.culture( $(this).val() );
-  $( "#spinner" ).timespinner( "value", current );
-});
 } );
