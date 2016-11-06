f033913e51c2f53a4143df3ef7ae52591d1e188a
Ela Korkmaz <elakorkmaz87@gmail.com>
2016-11-07 00:27:00 +0100

    voor merge

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var pug = require('pug');
 var app = express();
 
 app.use(express.static(__dirname +  '/'));
@@ -21,4 +22,41 @@ app.get('/a/*', function(req, res) {
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
+app.use(express.static(__dirname + '/'));
+});
+
+app.get('/', function(request, response) {
+  console.log('Requesting home page...');
+  response.send(pug.renderFile(__dirname + '/views/index.pug', {}));
+  // response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/yoga', function(request, response) {
+  console.log('Requesting contact page....');
+  response.send(pug.renderFile(__dirname + '/views/yoga.pug', {}));
+});
+
+app.get('/pricing', function(request, response) {
+  console.log('Requesting contact page....');
+  response.send(pug.renderFile(__dirname + '/views/pricing.pug', {}));
+});
+
+app.get('/appointment', function(request, response) {
+  console.log('Requesting contact page....');
+  response.send(pug.renderFile(__dirname + '/views/appointment.pug', {}));
+});
+
+app.get('/about', function(request, response) {
+  console.log('Requesting contact page....');
+  response.send(pug.renderFile(__dirname + '/views/about.pug', {}));
+});
+
+app.get('/contact', function(request, response) {
+  console.log('Requesting contact page....');
+  response.send(pug.renderFile(__dirname + '/views/contact.pug', {}));
+});
+
+
+app.listen(3000, function() {
+  console.log('Web server started on port 3000');
 });
